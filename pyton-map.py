c4335002cbdbc41bc6eb7c94881e71a4676c0326
gonzalogonzalo <calleriza.gonzalo@gmail.com>
2022-03-27 19:04:54 +0200

    Added volcano information to the map, need more data

--- pyton-map.py
+++ pyton-map.py
@@ -1,6 +1,18 @@
 import folium
+import pandas
+
+data = pandas.read_csv('volcanoes.text')
+lat = list(data['LAT'])
+lon = list(data['LON'])
+
 map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles = "Stamen Terrain")
 
-map.add_child(folium.Marker(location=[38.2, -99.1], popup='Hi, I am a Marker', icon=folium.Icon(color='green')))
+fg = folium.FeatureGroup(name='My Map')
+
+for lt, ln in zip(lat, lon):
+    fg.add_child(folium.Marker(location=[lt, ln], popup='Hi, I am a Volcano', icon=folium.Icon(color='yellow')))
+
+
+map.add_child(fg)
 
 map.save('Map1.html')
